4047444fc6eeb9b2d10844ab0023d6bc1a71a4d5
jeremychia <jeremyjchia@gmail.com>
2023-03-10 22:16:23 +0100

    clean: change to dates table (from version)

--- version.py
+++ version.py
@@ -11,4 +11,4 @@ version_fmt = [ 1 if date < cutoff else 2 for date in dates_fmt]
 
 version_df = pd.DataFrame({ 'Sitting_Date': dates_fmt, 'Version': version_fmt})
 
-version_df.to_csv(f'seeds/version.csv', index=False, mode='w')
+version_df.to_csv(f'seeds/dates.csv', index=False, mode='w')
