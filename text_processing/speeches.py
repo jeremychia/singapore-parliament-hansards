2dbd42f41f34e61282c529ad4e987ebd06ec9c28
Jeremy Chia <jeremyjchia@gmail.com>
2023-12-02 23:29:05 +0100

    feat: introductions to speech processing

--- text_processing/speeches.py
+++ text_processing/speeches.py
@@ -6,6 +6,15 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from gensim import corpora, models
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.model_selection import GridSearchCV
+# Evaluation Metrics
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from gensim.models import CoherenceModel
+import matplotlib.pyplot as plt
+
 
 def get_source_file_path(csv_subfolder):
     '''
@@ -25,7 +34,7 @@ def get_speech_file_names(source_file_path, dates_to_process):
 
     return temp
 
-def clean_text(text):
+def clean_text(text, additional_stopwords):
     # Convert to lowercase
     text = text.lower()
 
@@ -40,6 +49,8 @@ def clean_text(text):
 
     # Remove stop words
     stop_words = set(stopwords.words('english'))
+    stop_words.update(additional_stopwords)
+    
     tokens = [word for word in tokens if word not in stop_words]
 
     # Remove short words
@@ -73,13 +84,64 @@ def get_mp_name(x):
     else:
         return ''
 
+def intiailise_lda_model(cleaned_text, num_topics):
 
+    # Step 1: Tokenize the cleaned text column
+    tokenized_text = cleaned_text.apply(lambda x: x.split())
+
+    # Step 2: Create a dictionary representation of the documents
+    dictionary = corpora.Dictionary(tokenized_text)
+
+    # Step 3: Convert the tokenized documents into a document-term matrix
+    corpus = [dictionary.doc2bow(text) for text in tokenized_text]
+
+    # Step 4: Apply the LDA model
+    lda_model = models.LdaModel(corpus,\
+                                num_topics=num_topics,\
+                                id2word=dictionary,\
+                                passes=15,\
+                                random_state=42)
+
+    return lda_model, dictionary, corpus
+
+def evaluate_model(cleaned_text, n_components_range):
+
+    documents = cleaned_text.tolist()
+    
+    vectorizer = CountVectorizer()
+    dtm = vectorizer.fit_transform(documents)
+
+    perplexity_scores = []
+
+    for num_topics in n_components_range:
+
+        lda_model, _, corpus = intiailise_lda_model(cleaned_text, num_topics)
+
+        # Calculate perplexity score
+        perplexity_score = lda_model.log_perplexity(corpus)
+        print(f"{num_topics}: log perplexity = {perplexity_score}")
+        perplexity_scores.append(perplexity_score)
+
+    return perplexity_scores
+
+def plot_grid_search(n_components_range, perplexity_scores):
+
+    plt.plot(n_components_range, perplexity_scores, marker='o')
+    plt.xlabel('Number of Topics')
+    plt.ylabel('Log Perplexity')
+    plt.title('Optimal Number of Topics for LDA')
+    plt.show()
+
+    return 0
 
 ### Variables
 
 csv_subfolder = 'code_output'
 script_dir = os.path.dirname(os.path.abspath(__file__))
 dates_to_process = ['2023-11-22']
+additional_stopwords = ['singapore', 'speaker', 'minister', 'asked', 'question',\
+                        'please', 'whether', 'year', 'may']
+n_components_range = [i+1 for i in range(15)]
 
 ### Main run here
 
@@ -90,30 +152,22 @@ df = pd.read_csv(speech_files[0])
 
 ## Processing
 
-df['MP_Name'] = df['Speaker'].apply(get_mp_name)
-df['Cleaned_Text'] = df['Text'].apply(clean_text)
+df['mp_name'] = df['Speaker'].apply(get_mp_name)
+df['cleaned_text'] = df['Text']\
+                     .apply(lambda x: clean_text(x, additional_stopwords))
 
 ## Topics
 
-# Step 1: Tokenize the cleaned text column
-tokenized_text = df['Cleaned_Text'].apply(lambda x: x.split())
+perplexity_scores = evaluate_model(df['cleaned_text'], n_components_range)
+plot_grid_search(n_components_range, perplexity_scores)
 
-# Step 2: Create a dictionary representation of the documents
-dictionary = corpora.Dictionary(tokenized_text)
-
-# Step 3: Convert the tokenized documents into a document-term matrix
-corpus = [dictionary.doc2bow(text) for text in tokenized_text]
-
-# Step 4: Apply the LDA model
-lda_model = models.LdaModel(corpus, num_topics=5, id2word=dictionary, passes=15, random_state=42)
-
-# Step 5: Extract topics for each document
-df['Topics'] = df['Cleaned_Text'].apply(lambda x: lda_model[dictionary.doc2bow(x.split())])
+lda_model, dictionary, corpus = intiailise_lda_model(df['cleaned_text'], 5)
+df['topics'] = df['cleaned_text'].apply(lambda x: lda_model[dictionary.doc2bow(x.split())])
 
 for index, row in df.iterrows():
-    print(f"Document {index + 1}: {row['Cleaned_Text']}")
+    print(f"Document {index + 1}: {row['cleaned_text']}")
     print("Topics:")
-    for topic, score in row['Topics']:
+    for topic, score in row['topics']:
         print(f"  Topic {topic + 1}: {lda_model.print_topic(topic)} (Score: {score:.4f})")
     print("\n")
 
